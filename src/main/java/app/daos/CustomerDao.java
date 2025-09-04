f9f074642e39ee4296b9c45cd0df5702c531f400
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-09-04 19:02:20 +0200

    add deleted interface and added dao class

---
+++ src/main/java/app/daos/CustomerDao.java
@@ -0,0 +1,34 @@
+package app.daos;
+
+import app.entities.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+public class CustomerDao  {
+    private EntityManagerFactory emf;
+
+    public Customer create(Customer customer) {
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            em.persist(customer);
+            em.getTransaction().commit();
+        }
+        return customer;
+    }
+
+    public Customer findCustomerById(Integer id) {
+        try(EntityManager em = emf.createEntityManager()){
+            return em.find(Customer.class, id);
+        }
+    }
+
+
+    public List<Customer> getAll() {
+        try(EntityManager em = emf.createEntityManager()){
+            TypedQuery<Customer> query = em.createQuery("select c from Customer c", Customer.class);
+            return query.getResultList();
+        }
+    }
+}
